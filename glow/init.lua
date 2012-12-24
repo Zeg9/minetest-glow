5971c5051eb4ff19af82d5d3136a0f2b3130a0bb
Zeg9 <dazeg9@gmail.com>
2012-12-24 12:37:21 +0100

    Initial commit

---
+++ glow/init.lua
@@ -0,0 +1,35 @@
+-- glow/init.lua
+-- mod by john and Zeg9
+
+minetest.register_node("glow:stone", {
+	description = "Glowing stone",
+	tile_images = {"glow_stone.png"},
+	light_source = LIGHT_MAX,
+	groups = {cracky=3},
+})
+
+minetest.register_node("glow:lamp", {
+	description = "Lamp",
+	tile_images = {"glow_lamp.png"},
+	light_source = LIGHT_MAX,
+	groups = {cracky=3},
+})
+
+
+minetest.register_craft( {
+	output = '"glow:stone" 2',
+	recipe = {
+		{ 'default:stone','default:coal_lump','default:stone' }
+	},
+})
+
+minetest.register_craft( {
+	output = '"glow:lamp" 6',
+	recipe = {
+		{ 'default:stick','default:glass', 'default:stick' },
+		{ 'default:glass','glow:stone', 'default:glass' },
+		{ 'default:stick','default:glass', 'default:stick' },
+	},
+})
+
+minetest.register_alias("glow:lantern", "glow:lamp")
